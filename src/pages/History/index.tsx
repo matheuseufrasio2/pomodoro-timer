9aeefa0b2598c7aa8a3d1d90e2ae2e0f9a280dba
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-08-29 13:30:30 -0300

    styling history page

--- src/pages/History/index.tsx
+++ src/pages/History/index.tsx
@@ -1,13 +1,60 @@
 import {
-  Container,
+  HistoryContainer,
+  HistoryList,
+  Status,
 } from './styles';
 
 export function History() {
   return (
-    <Container>
+    <HistoryContainer>
       <h1>
-        History
+        Meu historico
       </h1>
-    </Container>
+
+      <HistoryList>
+        <table>
+          <thead>
+            <tr>
+              <th>Tarefa</th>
+              <th>Duracao</th>
+              <th>Inicio</th>
+              <th>Status</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha cerca de 2 horas</td>
+              <td>
+                <Status statusColor="yellow">
+                  Concluido
+                </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha cerca de 2 horas</td>
+              <td>
+                <Status statusColor="yellow">
+                  Concluido
+                </Status>
+              </td>
+            </tr>
+            <tr>
+              <td>Tarefa</td>
+              <td>20 minutos</td>
+              <td>Ha cerca de 2 horas</td>
+              <td>
+                <Status statusColor="yellow">
+                  Concluido
+                </Status>
+              </td>
+            </tr>
+          </tbody>
+        </table>
+      </HistoryList>
+    </HistoryContainer>
   );
 }
