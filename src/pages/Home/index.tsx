ce3781cc0e214c8498cf71d4499a725034f55744
Matheus Eufrasio <matheuseufrasio2@gmail.com>
2022-08-29 05:56:10 -0300

    styling home page

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,58 @@
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { Play } from 'phosphor-react';
 import {
-  Container,
+  CountdownContainer,
+  FormContainer,
+  HomeContainer,
+  MinutesAmountInput,
+  Separator,
+  StartCountdownButton,
+  TaskInput,
 } from './styles';
 
 export function Home() {
   return (
-    <Container>
-      <h1>
-        Home
-      </h1>
-    </Container>
+    <HomeContainer>
+      <form>
+        <FormContainer>
+          <label htmlFor="task">Vou trabalhar em</label>
+          <TaskInput
+            id="task"
+            placeholder="De um nome para o seu projeto"
+            list="task-suggestions"
+          />
+
+          <datalist id="task-suggestions">
+            <option value="Projeto 1">Projeto 1</option>
+            <option value="Projeto 2">Projeto 2</option>
+          </datalist>
+
+          <label htmlFor="minutesAmount">durante</label>
+          <MinutesAmountInput
+            type="number"
+            id="minutesAmount"
+            placeholder="00"
+            step={5}
+            min={0}
+            max={60}
+          />
+
+          <span>minutos.</span>
+        </FormContainer>
+
+        <CountdownContainer>
+          <span>0</span>
+          <span>0</span>
+          <Separator>:</Separator>
+          <span>0</span>
+          <span>0</span>
+        </CountdownContainer>
+
+        <StartCountdownButton type="submit">
+          <Play size={24} />
+          Comecar
+        </StartCountdownButton>
+      </form>
+    </HomeContainer>
   );
 }
--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,6 +1,6 @@
-import { createGlobalStyle } from "styled-components";
+import { createGlobalStyle } from 'styled-components';
 
-export const GlobalStyle =  createGlobalStyle`
+export const GlobalStyle = createGlobalStyle`
   * {
     margin: 0;
     padding: 0;
@@ -9,12 +9,13 @@ export const GlobalStyle =  createGlobalStyle`
 
   :focus {
     outline: 0;
-    box-shadow: 0 0 0 2px ${({theme}) => theme["green-500"]};
+    box-shadow: 0 0 0 2px ${({ theme }) => theme['green-500']};
   }
 
   body {
-    background: ${({theme}) => theme["gray-900"]};
-    color: ${({theme}) => theme["gray-300"]};
+    background: ${({ theme }) => theme['gray-900']};
+    color: ${({ theme }) => theme['gray-300']};
+    -webkit-font-smoothing: antialiased;
   }
 
   body, input, textarea, button {
